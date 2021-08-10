0f779a24ea2d36d596d2623ffb874f72992a7f23
hyuk <pjh08190819@naver.com>
2021-08-10 23:04:41 +0900

    add todo entity

---
+++ src/main/java/com/example/todo/repository/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.example.todo.repository;
+
+import com.example.todo.model.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+
+}
